18f4cc8216e249a395039c1a87c3d44da712f2b8
Damian Montero <photoca@hotmail.com>
2015-07-11 00:46:50 -0400

    init

--- app/src/main/java/sunshineweather/threeguys1phone/com/sunshine/MainActivity.java
+++ app/src/main/java/sunshineweather/threeguys1phone/com/sunshine/MainActivity.java
@@ -1,10 +1,9 @@
 package sunshineweather.threeguys1phone.com.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,8 +11,12 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
